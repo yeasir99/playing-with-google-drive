221f99b818361c08a20985fcd00443f030886235
Yeasir Ahamed <yahamed007@gmail.com>
2024-01-14 19:43:46 +0600

    add oauth

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
 import { google } from 'googleapis';
+import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+try {
+  const creds = fs.readFileSync('creds.json');
+  oauth2Client.setCredentials(JSON.parse(creds));
+} catch (error) {
+  console.log('No creds found');
+}
+
+app.get('/auth/google', (req, res) => {
+  const scopes = [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/drive',
+  ];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+  });
+  res.redirect(url);
 });
 
-const port = process.env.PORT || 8080;
+app.get('/auth/google/callback', async (req, res) => {
+  const { code } = req.query;
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+  fs.writeFileSync('creds.json', JSON.stringify(tokens));
+  res.send('Authenticated');
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
