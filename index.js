3194abe08b1093572f89f083044eb83b8e01c949
Yeasir Ahamed <yahamed007@gmail.com>
2024-01-14 22:46:12 +0600

    save text in google drive

--- index.js
+++ index.js
@@ -39,5 +39,24 @@ app.get('/auth/google/callback', async (req, res) => {
   res.send('Authenticated');
 });
 
+app.get('/save-text', async (req, res) => {
+  const drive = google.drive({ version: 'v3', auth: oauth2Client });
+  const textData =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+  await drive.files.create({
+    requestBody: {
+      name: 'Test',
+      mimeType: 'text/plain',
+    },
+    media: {
+      mimeType: 'text/plain',
+      body: textData,
+    },
+  });
+
+  res.status(200).send('File saved');
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
